effd3c8567422a4ae10617633d239854f20d4327
Thomas Kilgus <tomwaitforitmy@gmail.com>
2021-04-19 18:14:38 +0200

    First test and extra file to import from.

--- __tests__/parseThrones.spec.js
+++ __tests__/parseThrones.spec.js
@@ -1,4 +1,5 @@
-import { filterByTerm } from "../file";
+import { useCallback } from "react";
+import { filterByTerm, loadThrones } from "../file";
 
 describe("Filter function", () => {
   test("it should filter by a search term (link)", () => {
@@ -10,6 +11,19 @@ describe("Filter function", () => {
 
     const output = [{ id: 3, url: "https://www.link3.dev" }];
 
+    let x = 1;
+    let y = 2;
+
     expect(filterByTerm(input, "link")).toEqual(output);
+  });
+
+  test("get some json/html", () => {
+    const input = "https://www.thronemaster.net/?goto=gaming&sub=my_games";
+
+    const output = "???";
+
+    loadThrones(input, () => {
+      console.log("test");
+    });
   });
 });
--- file.js
+++ file.js
@@ -3,3 +3,27 @@ export function filterByTerm(inputArr, searchTerm) {
     return arrayElement.url.match(searchTerm);
   });
 }
+
+export function loadThrones(link) {
+  const https = require("https");
+
+  https
+    .get(link, (resp) => {
+      let data = "";
+
+      // A chunk of data has been received.
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
+        console.log(JSON.parse(data).explanation);
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+
+  console.log("tommy");
+}
